83be02faf8ec0ddaa3cf3d4d20141f2187740593
liyacommits <liyabonamendela3@gmail.com>
2026-06-06 14:50:09 +0200

    backend set up

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,3 +9,14 @@ app = FastAPI(
     docs_url="/docs",
     recdocs_url="/redoc",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True
