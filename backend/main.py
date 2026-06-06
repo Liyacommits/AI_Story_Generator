10b445b88a7b272ae2ab5c61000744aca9724543
liyacommits <liyabonamendela3@gmail.com>
2026-06-06 20:08:44 +0200

    schemas set up

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
 app = FastAPI(
     title= "Choose your own adventure API",
     description= "An API for a choose your own adventure game",
@@ -12,11 +14,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
